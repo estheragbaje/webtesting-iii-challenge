54c1fc7563f3e56574ec40c6d19ae016999db159
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-14 15:00:59 +0100

    Tested Dashboard and Gate

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,30 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Controls />);
+});
+
+describe("Controls Component", () => {
+  it("shows default open gate", () => {
+    // const openCloseButton = tools.queryByTestId("openCloseControl");
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+    expect(wrapper.queryByText(/open/i)).not.toBeInTheDocument();
+    expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+  });
+  
+//   it("shows default open gate", () => {
+//     // const openCloseButton = tools.queryByTestId("openCloseControl");
+//     expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+//     rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+//     expect(wrapper.queryByText(/open/i)).not.toBeInTheDocument();
+//     expect(wrapper.queryByText(/closed/i)).toBeInTheDocument();
+//   });
+// });
